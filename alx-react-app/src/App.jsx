27cf71736e1d0b3cc45ba5badbec1e10b8692ff5
Martin Kimani <martinikimi7@gmail.com>
2026-01-20 17:16:07 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <WelcomeMessage />
-    </>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
