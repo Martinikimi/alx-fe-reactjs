041347b18327c753438489503cfc4a122d5ef503
Martin Kimani <martinikimi7@gmail.com>
2026-01-30 13:40:09 +0300

    Create App.jsx

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search Application</h1>
+      <p>Ready for development!</p>
+    </div>
+  )
+}
+
+export default App
