67f714ac4bcaf86d68dde7795f53ac1b1aa381a3
Martin Kimani <martinikimi7@gmail.com>
2026-02-22 23:34:31 +0300

    Create App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Layout from './components/Layout';
+import Login from './components/Login';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import Profile from './pages/Profile';
+import ProfileDetails from './pages/profile/ProfileDetails';
+import ProfileSettings from './pages/profile/ProfileSettings';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          {/* Public routes with Layout */}
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="blog/:postId" element={<BlogPost />} />
+            
+            {/* Protected routes */}
+            <Route path="profile" element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }>
+              <Route index element={<Navigate to="details" replace />} />
+              <Route path="details" element={<ProfileDetails />} />
+              <Route path="settings" element={<ProfileSettings />} />
+            </Route>
+          </Route>
+          
+          {/* Public auth route */}
+          <Route path="/login" element={<Login />} />
+          
+          {/* Catch all - 404 redirect */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
